407a781ceee6e0867147d83fd70914a78ba86663
sdc00039@gamil.com <sdc00035@naver.com>
2022-10-12 00:26:01 +0900

    '1012'

--- gold/16234_인구이동.py
+++ gold/16234_인구이동.py
@@ -1,85 +1,48 @@
+'''
+결과 : 인구이동이 없으면 끝낸다 => 기존값을 유지하고 있어야함   
+4방향 델타에서 L <= 인구 <= R 이 있는지를 확인한다.
+있다면 경계선을 풀어준다. => 어떤 특정 리스트에 담아주는 것이 좋아보인다? 
+'''
 import sys
-sys.setrecursionlimit(10**4) # 깊이를 변화시켜준다.
-<<<<<<< HEAD
-=======
-from copy import deepcopy
->>>>>>> 74c686e564d6699fd652b4474c7da5e73c370ccd
+input = sys.stdin.readline
 
-def dfs(i,j):
-    global count, visited,count_count_dic
-    visited[i][j] = arr_count
-    if visited[i][j] not in count_count_dic:
-        count_count_dic[visited[i][j]] = 1
-    else:
-        count_count_dic[visited[i][j]] += 1
-    count += arr[i][j]
-    if 0 <= i <= N-1 and 0 <= j <= N-1:
-        if j+1 < N:
-            if L <= abs(arr[i][j] - arr[i][j+1]) <= R and visited[i][j+1] == 0: # 뒤에부분...
-                dfs(i,j+1)
-        if j-1 >= 0:
-            if L <= abs(arr[i][j] - arr[i][j-1]) <= R  and visited[i][j-1] == 0:
-                dfs(i,j-1)
-        if i-1 >= 0:
-            if L <= abs(arr[i][j] - arr[i-1][j]) <= R  and visited[i-1][j] == 0:
-                dfs(i-1,j)
-        if i+1 < N:
-            if L <= abs(arr[i][j] - arr[i+1][j]) <= R  and visited[i+1][j] == 0:
-                dfs(i+1,j)
-    else:
-        return 
+def solution(i,j):
+    global unite_sum
+    unite = [(i,j)]
+    queue = [(i,j)]
+    while queue:
+        i, j = queue.pop()
+        for di, dj in ((1,0),(-1,0),(0,1),(0,-1)):
+            ni = i + di
+            nj = j + dj
+            if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj] and L <= abs(graph[i][j] - graph[ni][nj]) <= R:
+                visited[ni][nj] = 1
+                queue.append((ni,nj))
+                unite.append((ni,nj))
+                unite_sum += graph[ni][nj] # 해당 인구를 확인한다.
+    return unite
 
 N, L, R = map(int, input().split())
-arr = [list(map(int, input().split())) for i in range(N)]
-<<<<<<< HEAD
+graph = [list(map(int,input().split())) for _ in range(N)]
 
-day = 0 # 처음날짜부터 얼만큼이나 돌아갔는가?
+time = 0
 while True:
-=======
-day = 0 # 처음날짜부터 얼만큼이나 돌아갔는가?
-while True:
-    a = deepcopy(arr)
->>>>>>> 74c686e564d6699fd652b4474c7da5e73c370ccd
-    count_count_dic = {}
-    count_list = {}
-    day += 1
-    visited = [[0] * N for _ in range(N)] # 방문했는지 안했는지를 확인하는 방법
-    arr_count = 0
+    time += 1
+    visited = [[0] * N for _ in range(N)] # 방문은 항상 바뀔 수 있다.
     count = 0
     for i in range(N):
         for j in range(N):
-            if visited[i][j] != 0:
-                continue
-            else:
-                # 해당되는 영역을 구분하기 위하여 사용
-                arr_count += 1
-                count = 0 # 해당 영역에서의 합을 구한다.
-                dfs(i,j)
-<<<<<<< HEAD
-                count_list[arr_count] = count # 해당 영역과 영역의 합을 구한다.
-    # print(visited)
-    # print(count)
-    # print(count_list)
-    if visited[-1][-1] == N*N:
-        print(day-1)
-        break
-    else:
-    # 해당되는 구분에 따라서 arr를 변경하기 위해서 사용한다.
-        for i in range(N):
-            for j in range(N):
-                if visited[i][j] in count_list:
-                    arr[i][j] = count_list[visited[i][j]] // count_count_dic[visited[i][j]]
-=======
-                count_list[arr_count] = count
-            
-                arr[i][j] = count // count_count_dic[visited[i][j]]
-            # 해당되는 구분에 따라서 arr를 변경하기 위해서 사용한다.
-    print(visited)
-    print(arr)
+            if not visited[i][j]: # 방문을 안했다면 해준다.
+                visited[i][j] = 1
+                unite_sum = graph[i][j] # 인구의 합을 구해준다.
+                unite = solution(i,j)
+                if len(unite) == 1:
+                    count +=1
+                else:
+                    popul = unite_sum // len(unite) # 나눈것을 의미한다.
+                    for u_i, u_j in unite: # 연합되어있는 index를 넣는다.
+                        graph[u_i][u_j] = popul
     
-    if a != arr:
-        continue
-    else:
-        print(day-1)
+    if count == N*N:
+        print(time-1)
         break
->>>>>>> 74c686e564d6699fd652b4474c7da5e73c370ccd
