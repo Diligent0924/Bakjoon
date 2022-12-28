e8cad3340d04860cf05d78ea304dd619e921fbf3
PPPARK <sdc00039@gmail.com>
2022-12-29 00:08:29 +0900

    change

--- 실버/BOJ1235.py
+++ 실버/BOJ1235.py
@@ -1,19 +1,17 @@
+import sys
 N = int(input())
-list_a = []
 list_a = [input()[::-1] for _ in range(N)]
 len_word = len(list_a[0])
 
-for i in range(len_word):
-    ver = True
+for i in range(1,len_word+1):
     list_b = []
     for j in range(N):
         str1 = list_a[j][:i]
         if str1 in list_b:
-            ver = False
             break
         else:
             list_b.append(str1)
-           
+    # print(list_b)         
     if len(list_b) == N:
         print(i)
         break
