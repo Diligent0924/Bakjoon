8cc3ac81f849d54e788e78e5ab7909e65d55953e
PPPARK <sdc00039@gmail.com>
2023-01-08 23:47:22 +0900

    BOJ2156

--- 실버/BOJ2156.py
+++ 실버/BOJ2156.py
@@ -1,9 +1,18 @@
+# DP는 현재의 위치를 먹었을 때의 최댓값으로 구한다.
 N = int(input())
 arr = [int(input()) for i in range(N)]
-podoju = [0] * N # N개의 일렬로 정의된 포도주들
-podoju[0] = arr[0]
-podoju[1] = arr[0] + arr[1]
-
-for i in range(2, len(arr)): # 해당 인덱스 값이 되었을 경우 
-    podoju[i] = podoju[i-2] + arr[i]
-print(podoju)
+# print(arr)
+DP = [0] * N # 바로 앞칸으로 한 경우
+for i in range(N):
+    if i == 0:
+        DP[0] = arr[0]
+    elif i == 1:
+        DP[1] = arr[0] + arr[1]
+    elif i == 2:
+        DP[2] = max(arr[0], arr[1]) + arr[2]
+        DP[2] = max(DP[2],DP[1])
+    else: 
+        DP[i] = arr[i] + max(DP[i-2], arr[i-1]+DP[i-3])
+        DP[i] = max(DP[i], DP[i-1])
+# print(DP)
+print(max(DP))
