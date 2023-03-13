a54d196650174f03411cb6cb346009d4dca1e2f2
Diligent0924 <sdc00039@gmail.com>
2023-03-13 17:57:28 +0900

    15685_드래곤커브

--- gold/177220_마약수사대.py
+++ gold/177220_마약수사대.py
@@ -1,7 +1,17 @@
 '''
-단방향으로 확인이 가능하다.
-A, B, C, D를 각각 0, 1과 같이 순서대로 넣는다. ord("A") - 65 방식을 사용
-1. Root 
+문제점
+  - 문제에 대한 해석에 대한 에러가 있었다.
+  - 나의 경우 부모 및 자식 노드를 모두 버리게끔 사용했으나 자식기준에서 부모가 2명이라면 마약이 공급된다는 것을 간과함
+  - 먀약 공급원은 결국 root 마약에게 약을 받아야한다는 개념을 알아야함
+
+풀이방법
+  - root 마약원을 확인함 => 자식 기준에서 Dictionary를 통하여 없다면 root 마약원임
+  - 검거된 노드들의 간선을 모두 제거해야함 (만약 검거된 노드만 부모 노드인 자식 노드들은 기존 root와는 따로 떨어짐)
+  - root에서부터 시작하여 연결된 노드들의 개수를 확인함
+  
+소감
+  - 그래프의 경우 어떤 부분이든 연결이 된다는 것을 생각하고 문제를 풀어야 한다.
+  - 하나의 기준이 아닌 반대쪽 기준에서도 확인을 해야 한다.
 '''
 from collections import defaultdict
 def ord_change(alpha):
@@ -48,7 +58,7 @@ for i in root:
         visited[i] =1
         while queue:
             node = queue.pop(0)
-            print(node)
+            # print(node)
             for h in arr[node]: # 갈 수 있는 곳을 한번씩 확인한다.    
                 if not visited[h]: 
                     visited[h] = 1
